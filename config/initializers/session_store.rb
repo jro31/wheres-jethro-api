5148f85bc8099390b788d1fd24ecb91b2ca550f2
Jethro Williams <jethrowilliams@outlook.com>
2022-02-09 09:34:01 +0000

    Remove comments

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == 'production'
-  Rails.application.config.session_store :cookie_store, key: "_wheres_jethro", domain: "api.wheresjethro.com" # TODO - Update this to whatever the API URL is
+  Rails.application.config.session_store :cookie_store, key: "_wheres_jethro", domain: "api.wheresjethro.com"
 else
   Rails.application.config.session_store :cookie_store, key: "_wheres_jethro"
 end
